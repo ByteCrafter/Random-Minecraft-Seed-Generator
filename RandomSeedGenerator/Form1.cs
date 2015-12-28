40b7f519f91af6ac8664c514f90f33ca99a3fa2a
ByteCrafter <liveDanielevil@live.de>
2015-12-28 18:02:34 +0100

    Initial Commit

---
+++ RandomSeedGenerator/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RandomSeedGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked == true)
+            {
+                textBox1.Text = Generator.SeedGenerator(Generator.SeedOption.Small).ToString();
+                MessageBox.Show(textBox1.Text);
+                Clipboard.SetText(textBox1.Text);
+            }
+            else if (radioButton2.Checked == true)
+            {
+                textBox1.Text = Generator.SeedGenerator(Generator.SeedOption.Big).ToString();
+                MessageBox.Show(textBox1.Text);
+                Clipboard.SetText(textBox1.Text);
+            }
+
+            }
+    }
+
+
+
+    }
+
---
+++ RandomSeedGenerator/Generator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomSeedGenerator
+{
+    class Generator
+    {
+        public static Int64 SeedGenerator(SeedOption option)
+        {
+            if (option == SeedOption.Small)
+            {
+                Random rnd = new Random();
+                var buffer = new byte[sizeof(Int32)];
+                rnd.NextBytes(buffer);
+                return BitConverter.ToInt32(buffer, 0);
+            }
+            else if (option == SeedOption.Big)
+            {
+                Random rnd = new Random();
+                var buffer = new byte[sizeof(long)];
+                rnd.NextBytes(buffer);
+                return BitConverter.ToInt64(buffer, 0);
+            }
+            return 0;
+        }
+
+
+        public enum SeedOption
+        {
+            Small,
+            Big
+        }
+
+    }
+
+}
